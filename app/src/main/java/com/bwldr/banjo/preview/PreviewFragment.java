67742c618ff4d2de9ac22c9d583baf2f74fdf288
aronysidoro <pyaaron@gmail.com>
2017-05-14 07:00:25 -0700

    [save picture and broadcast] WIP - spike worked. Need to refactor location of logic and test

--- app/src/main/java/com/bwldr/banjo/preview/PreviewFragment.java
+++ app/src/main/java/com/bwldr/banjo/preview/PreviewFragment.java
@@ -1,19 +1,34 @@
 package com.bwldr.banjo.preview;
 
+import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bwldr.banjo.R;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class PreviewFragment extends Fragment {
+
+    private static final String TAG = PreviewFragment.class.getSimpleName();
+
+    private Button mSaveButton;
 
     public PreviewFragment() {
     }
@@ -28,9 +43,94 @@ public class PreviewFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_preview, container, false);
 
-        Uri photoUri = Uri.parse(getArguments().getString("data"));
+        final Uri photoUri = Uri.parse(getArguments().getString("data"));
         ((ImageView) view.findViewById(R.id.image)).setImageURI(photoUri);
 
+        mSaveButton = (Button) view.findViewById(R.id.button);
+        mSaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                broadcastNewFile(photoUri);
+            }
+        });
+
         return view;
+    }
+
+    private void broadcastNewFile(Uri uri) {
+        File inputFile = new File(uri.getPath());
+        String inputPath = inputFile.getAbsolutePath();
+        File outputFile = getFileFromUri(uri);
+        String outputPath = outputFile.getPath();
+
+        copyFiletoProtoDir(inputPath, outputPath);
+
+        MediaScannerConnection.scanFile(
+                getActivity(),
+                new String[]{outputFile.getPath()},
+                null,
+                new MediaScannerConnection.OnScanCompletedListener() {
+                    @Override
+                    public void onScanCompleted(String s, Uri uri) {
+                        Log.d(TAG, "media scan completed");
+                    }
+                }
+        );
+    }
+
+    private File getFileFromUri(Uri uri) {
+        String fileName = uri.getLastPathSegment();
+        File picturesDir =
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File outputDir = new File(picturesDir, getActivity().getString(R.string.app_name));
+        return new File(outputDir, fileName);
+    }
+
+    private void copyFiletoProtoDir(String inputPath, String outputPath) {
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+            //create output directory if it doesn't exist
+            getPhotoDirectory();
+
+            in = new FileInputStream(inputPath);
+            out = new FileOutputStream(outputPath);
+
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = in.read(buffer)) != -1) {
+                out.write(buffer, 0, read);
+            }
+            in.close();
+            in = null;
+
+            // write the output file (You have now copied the file)
+            out.flush();
+            out.close();
+            out = null;
+
+        }  catch (FileNotFoundException fnfe1) {
+            Log.e("tag", fnfe1.getMessage());
+        }
+        catch (Exception e) {
+            Log.e("tag", e.getMessage());
+        }
+    }
+
+    private File getPhotoDirectory() {
+        File outputDir = null;
+        String externalStorageState = Environment.getExternalStorageState();
+        if (externalStorageState.equals(Environment.MEDIA_MOUNTED)) {
+            File picturesDir =
+                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+            outputDir = new File(picturesDir, getActivity().getString(R.string.app_name));
+            if (!outputDir.exists()) {
+                if (!outputDir.mkdirs()) {
+                    Log.e(TAG, "outputDir.mkdirs: error");
+                    outputDir = null;
+                }
+            }
+        }
+        return outputDir;
     }
 }
---
+++ app/src/main/java/com/bwldr/banjo/preview/PreviewPresenter.java
@@ -0,0 +1,8 @@
+package com.bwldr.banjo.preview;
+
+/**
+ * Created by aaron on 5/14/17.
+ */
+
+public class PreviewPresenter {
+}
